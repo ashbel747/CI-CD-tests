13a04972ca87467260972bd9fe65f45f58c60e63
Florence Ndemi <ndemiflorence@gmail.com>
2025-09-05 12:20:16 +0300

    Added guards unit tests

--- backend-app/src/app.controller.spec.ts
+++ backend-app/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "NestJS Auth API Service"', () => {
+      expect(appController.getHello()).toBe('NestJS Auth API Service');
     });
   });
 });
